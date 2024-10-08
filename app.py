4073933fea049bd1ee5146a902e1328f134a5719
nelsonelaye <nelsonelaye@gmail.com>
2024-10-08 18:05:30 +0100

    refactor: add waitress to requirements file
    define application port

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def start_chat():
         return jsonify({"error": "An internal error occurred", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
